7fd1878703c5006c29b15f1f4db3339b70320768
Matthew Obrien <mattewobrien77@protonmail.com>
2020-08-19 17:23:32 +0300

    Updates

---
+++ precache-manifest.66f9ebffb0c1d0334ce85688031eee58.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237ef8cad27796e7f1011feb38d3f3dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7128afb70546cb56204f",
+    "url": "/static/css/main.e31dbf66.chunk.css"
+  },
+  {
+    "revision": "18b229c03b702972ce10",
+    "url": "/static/js/2.0b5c5ac2.chunk.js"
+  },
+  {
+    "revision": "1176b48ba521377fd1828bcc6ff908eb",
+    "url": "/static/js/2.0b5c5ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7128afb70546cb56204f",
+    "url": "/static/js/main.cc59f896.chunk.js"
+  },
+  {
+    "revision": "6337c631b4b0c18f6c59",
+    "url": "/static/js/runtime-main.d273c1f2.js"
+  },
+  {
+    "revision": "2e20d8e680c162dd27fe6492af7ff837",
+    "url": "/static/media/earth.2e20d8e6.png"
+  },
+  {
+    "revision": "57a7b2a2b8f9a737cb174452b174177d",
+    "url": "/static/media/faucet.57a7b2a2.png"
+  },
+  {
+    "revision": "5124fadfe2dbc5e2d7406bf8fa2861b5",
+    "url": "/static/media/mint.5124fadf.png"
+  },
+  {
+    "revision": "57f5652c29c8823d848b3613deb04a89",
+    "url": "/static/media/monetary.57f5652c.png"
+  },
+  {
+    "revision": "aa3df3780d16d1d00dee23691e582349",
+    "url": "/static/media/supply.aa3df378.png"
+  }
+]);
